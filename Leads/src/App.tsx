d5863cb199c12151f3795b270bb8601b33c5c0e4
GUILHERME MATHEUS LIESENFELD <guilhermeliesenfeld8@gmail.com>
2024-04-30 09:55:45 -0300

    initial configs project

--- Leads/src/App.tsx
+++ Leads/src/App.tsx
@@ -1,11 +1,13 @@
 import logo from './assets/logo.svg'
+import './index.css'
 
 function App() {
   return (
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
+      <div className='grid h-screen grid-cols-2 place-items-center'>
+        <div className='flex items-center gap-4'>
           <img src={logo} className="logo" alt="ACERTA logo" />
-        </a>
+          <h1 className='text-2xl font-sans text-black-text'>Consulta de Leads </h1>
+        </div>
       </div>
   )
 }
