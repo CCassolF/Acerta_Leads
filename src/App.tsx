c811ad9407c3547c25c4c84e4c718e5a79328ea2
GUILHERME MATHEUS LIESENFELD <guilhermeliesenfeld8@gmail.com>
2024-05-14 19:13:30 -0300

    add git

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ export function App( ) {
   return (
     <div className="bg-slate-100 h-screen">
       <HeaderCadastro/>
-      <div className="bg-white mx-36 mt-7 p-6 border w-auto rounded-lg ">
-        <div className="border h-16 border-cyan-500 w-[460px] flex justify-center space-x-44">
+      <div className="flex justify-center bg-white mx-36 mt-7 p-6 border w-auto rounded-lg ">
+        <div className="border h-16  border-cyan-500 w-[460px] flex justify-center space-x-44">
           <h1>Dados pessoais</h1>
           <h1>Contato</h1>
           </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,25 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [],
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    fontFamily: {
+      'source-sans-3': '"Source Sans 3", sans-serif',
+    },
+  extend: {
+    colors: {
+      'black-text': '#252433',
+      'cyan-button': '#198CFF',
+      'gray-text':' #B9C2CB',
+      'gray-table': '#71727D',
+      'gray-head-table': '#4D4F5C'
+    },
   },
-  plugins: [],
+  plugins: 
+  [('@tailwindcss/forms')
+  ],
+ 
+},
 }
-
